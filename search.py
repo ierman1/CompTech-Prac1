64959237ac8f79bda6e6d431182b7348188fd569
w2.gperez <ierman1@hotmail.com>
2021-10-05 15:34:31 +0200

    Comentaris i codi per debugar

--- search.py
+++ search.py
@@ -21,13 +21,30 @@ def BFS(lab:Labyrinth):
 
     while len(pending) != 0:
         
+        # =================== DEBUG =================== #
+        print("\n======\nChecking => " + pending[0].__repr__() + "\nPending at the start => ", end = '')
+        for p in pending:
+            print(p.__repr__() + " ", end = '')
+        # =================== DEBUG =================== #
+
+        '''
+            1. - Guardem la primera posició de la llista de pendents i l'eliminem de la llista
+            2. - Iterem per les caselles veínes, i si són vàlides, les desem a pendents
+        '''
+
         tmp = pending[0]                    # tmp = [(1,1)] => [(1,2)]
         visited.append(pending[0])          # visited = [(1,1)] => [(1,1), (1,2)]
         pending = pending[1:]               # pending = [] => [(2,1)]
 
-        for c in tmp:
+        for c in tmp.getChildren():
             if c not in visited:
                 pending.append(c)           # pending = [(1,2), (2,1)] => [(2,1),(1,3)]
+
+        # =================== DEBUG =================== #
+        print("\nPending at the end => ", end = '')
+        for p in pending:
+            print(p.__repr__() + " ", end = '')
+        # =================== DEBUG =================== #
 
     return trail
 
